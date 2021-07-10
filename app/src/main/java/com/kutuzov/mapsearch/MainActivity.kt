87426a7563753da9170b9afe9d6a084bb0c013c8
Kuzya1991 <o.kuzubov@Kuzya1991s-MacBook-Pro.local>
2021-07-10 16:40:21 +0200

    Add main tomtom map fragment

--- app/src/main/java/com/kutuzov/mapsearch/MainActivity.kt
+++ app/src/main/java/com/kutuzov/mapsearch/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.kutuzov.mapsearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kutuzov.mapsearch.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.tomtom.online.sdk.map.*
+import com.kutuzov.mapsearch.R
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+
+        initAppBar()
+    }
+
+    private fun initAppBar() {
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navGraph = navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
     }
 }
--- app/src/main/java/com/kutuzov/mapsearch/ui/main/MainFragment.kt
+++ app/src/main/java/com/kutuzov/mapsearch/ui/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.kutuzov.mapsearch.ui.main
 
+import android.location.Location
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kutuzov.mapsearch.R
+import com.tomtom.online.sdk.location.LocationUpdateListener
+import com.tomtom.online.sdk.map.*
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), OnMapReadyCallback, LocationUpdateListener {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -24,7 +27,80 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        initMap(savedInstanceState)
     }
 
+    private lateinit var tomtomMap: TomtomMap
+    private lateinit var mapView: MapView
+
+    private fun initMap(savedInstanceState: Bundle?) {
+        val keysMap = mapOf(
+            ApiKeyType.MAPS_API_KEY to "MAhsJKGvYXB3Org5v3Z2JOJlg6lP47cZ"
+        )
+
+        val properties = MapProperties.Builder()
+            .keys(keysMap)
+            .build()
+
+        val mapFragment = MapFragment.newInstance(properties)
+        mapView = MapView(requireContext())
+
+        if (savedInstanceState == null) {
+            childFragmentManager.beginTransaction()
+                .replace(R.id.container, mapFragment)
+                .commitNow()
+        }
+
+        mapFragment.getAsyncMap(this)
+    }
+
+
+    override fun onMapReady(tomtomMap: TomtomMap) {
+        this.tomtomMap = tomtomMap
+        tomtomMap.addLocationUpdateListener (this)
+        tomtomMap.isMyLocationEnabled = true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        tomtomMap.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onHostSaveInstanceState()
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        tomtomMap.userLocation?.let {
+            tomtomMap.centerOnMyLocationWithNorthUp()
+            tomtomMap.removeLocationUpdateListener(this)
+        }
+    }
 }
