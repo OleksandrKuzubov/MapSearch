d41a23a49eceae63358d30e5f280844c253fdf6c
Kuzya1991 <o.kuzubov@Kuzya1991s-MacBook-Pro.local>
2021-07-15 22:19:25 +0200

    Add favorite viewModel unit tests

--- domain/src/main/java/com/kutuzov/domain/model/FavoriteAddress.kt
+++ domain/src/main/java/com/kutuzov/domain/model/FavoriteAddress.kt
@@ -6,13 +6,13 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class FavoriteAddress(
     val id: String,
-    val streetNumber: String?,
-    val street: String?,
-    val streetName: String?,
-    val municipality: String?,
-    val postalCode: String?,
-    val countryCode: String?,
-    val country: String?,
-    val freeFormAddress: String?,
+    val streetNumber: String? = null,
+    val street: String? = null,
+    val streetName: String? = null,
+    val municipality: String? = null,
+    val postalCode: String? = null,
+    val countryCode: String? = null,
+    val country: String? = null,
+    val freeFormAddress: String? = null,
     var isExpanded: Boolean = false
 ) : Parcelable
---
+++ presentation/src/test/java/com/kutuzov/mapsearch/KoinTestApp.kt
@@ -0,0 +1,6 @@
+package com.kutuzov.mapsearch
+
+import android.app.Application
+
+class KoinTestApp : Application() {
+}
